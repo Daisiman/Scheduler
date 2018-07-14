d08aa4c1f6bfe928e1b10bdc1a0d91435a2eedf6
Dainius <dainius.simanavicius@gmail.com>
2018-07-15 01:38:40 +0300

    Add OverrideDoctorTable migration

--- Scheduler/Models/Doctor.cs
+++ Scheduler/Models/Doctor.cs
@@ -11,6 +11,6 @@ namespace Scheduler.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        //public string Scope { get; set; }
+        public string Scope { get; set; }
     }
 }
