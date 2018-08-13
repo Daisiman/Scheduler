1f41b4ff006652a5135f03c3b996e7b952674218
Dainius <dainius.simanavicius@gmail.com>
2018-08-14 01:02:59 +0300

    Minor DB changes

--- Scheduler/Data/ApplicationDbContext.cs
+++ Scheduler/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Scheduler.Data
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<DoctorWorkHours> DoctorWorkHours { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<DoctorImage> DoctorImages { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
